fa05c765038f3d68d515f3ed103547074c413140
codelink7 <codelink7@gmail.com>
2022-07-31 09:35:15 +0200

    anoter commit

--- src/Hello world.c
+++ src/Hello world.c
@@ -5,7 +5,9 @@ int main(void)
 
     int i =0;
     for(i = 1; i <= 5; i ++)
-    printf("Blah (%d)\n", i);
+    printf("Blah (%d)\n", i);       // print 'blah' 5 times and count them
+
+
 
 
     return 0;
