81c116e3b65720e535a4b09883a8cb4214fc1797
codelink7 <codelink7@gmail.com>
2023-02-27 15:08:28 +0200

    remembered, moved files to organize space

---
+++ C/src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(void)
+{
+
+
+    
+    char string[100];
+    int i, len, flag = 0;
+    printf("Enter to check palindrome:\n");                     // allow user to enter string
+    scanf("%s", string);                                        // takes string as a input
+    len = strlen(string);                                       // counts string length
+    for(i = 0; i < len; i++)
+    {
+        if(string[i] != string[len-i-1])
+        {
+             flag = true;
+            break;
+        }
+    }
+        if(flag)
+        {
+            printf("Input is not palindrome\n");
+        }
+        else {
+            printf("Input is a palindrome\n");
+        }
+    
+    return 0;
+}
