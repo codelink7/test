8a670efc966e66a167261afe145065e491146878
codelink7 <codelink7@gmail.com>
2023-03-05 01:26:37 +0200

    updated

--- C/src/main.c
+++ C/src/main.c
@@ -1,32 +1,107 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+
+int power (int base, int exp)
+{
+
+	int result = 1;
+	if(exp > 0)
+	{
+		while(exp!=0)
+		{
+			result = base * result;
+			exp--;
+		}
+
+		return result;
+
+	}
+
+	
+
+	else
+		return 0;
+
+
+}
+
+
+
 
 int main(void)
 {
 
+	int input = 0;
+	int num = 0;
+	int Digit_num;
+	int Flag = 0;
+
+	
+
+	printf("enter number \n");
+
+	scanf("%d", &input);
+
+	num = input;
+	
+	for(Digit_num = 0; num >0; Digit_num++)
+	{
+		num = num/10;
+	}
+	num = input;
+	int Array[Digit_num];
+	int Digit = Digit_num;
+
+	printf("number of digits = %d \n", Digit_num);
+
+
+	for(int i = 0;Digit_num>=1; Digit_num-- && i++)
+	{
+		if(Digit_num == 1)
+		{
+			Array[i] = num;
+			break;
+		}
+
+		Array[i] = num/power(10,Digit_num-1);
+		num = num%power(10,Digit_num-1);
+
+		
+				
+
+	}
+		for(int i = 0; i<Digit; i++)
+		{
+
+			printf("%d\n", Array[i]);
+		}
+
+
+	for(int i = 0; i <(Digit-1)/2; i++)
+	{
+		if(Array[i] != Array[Digit-1-i])
+		{
+			Flag = 1;
+		}
+		
+	}
+
+	if(Flag == 1)
+	{
+		printf("Number is not palindrome\n");
+
+
+	}
+
+	else
+		printf("Number is palindrome\n");
+
+	
+	
+
 
     
-    char string[100];
-    int i, len, flag = 0;
-    printf("Enter to check palindrome:\n");                     // allow user to enter string
-    scanf("%s", string);                                        // takes string as a input
-    len = strlen(string);                                       // counts string length
-    for(i = 0; i < len; i++)
-    {
-        if(string[i] != string[len-i-1])
-        {
-             flag = true;
-            break;
-        }
-    }
-        if(flag)
-        {
-            printf("Input is not palindrome\n");
-        }
-        else {
-            printf("Input is a palindrome\n");
-        }
+   return 0;
+      
     
-    return 0;
+    
 }
